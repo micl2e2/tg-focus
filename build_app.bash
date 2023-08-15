af53abd97faa712803499c7e1a8c69241cb12729
Michael Lee <micl2e2@proton.me>
2023-08-15 22:33:47 +0800

    fix log pos

--- build_app.bash
+++ build_app.bash
@@ -6,10 +6,10 @@ buildah copy --from tdlib-builder-container app-tgfocus-container '/usr/local/li
 # cwd is /
 buildah copy --from tgfocus-builder-container app-tgfocus-container '/tg-focus/target/release/tg-focus' '/'
 
-buildah run app-tgfocus-container -- bash -c 'mkdir .tg-focus'
+# buildah run app-tgfocus-container -- bash -c 'mkdir /tmp/tg-focus'
 
 buildah copy app-tgfocus-container 'tgfocus-white.jpg' '/'
 
-buildah config --cmd "/bin/bash -c ' LD_LIBRARY_PATH=/usr/local/lib /tg-focus >.tg-focus/stdout 2>.tg-focus/stderr'" app-tgfocus-container
+buildah config --cmd "/bin/bash -c ' LD_LIBRARY_PATH=/usr/local/lib /tg-focus >/tmp/tg-focus/stdout 2>/tmp/tg-focus/stderr'" app-tgfocus-container
 
 buildah commit app-tgfocus-container tg-focus
