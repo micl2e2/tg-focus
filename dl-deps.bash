acbc000c511b4bd19ebe111ed4e1c42952d09d2c
Michael Lee <micl2e2@proton.me>
2023-09-19 14:13:09 +0800

    refine prev commit

--- dl-deps.bash
+++ dl-deps.bash
@@ -1,8 +1,11 @@
-
 mkdir -p deps
 
-git clone --quiet https://github.com/ToruNiina/toml11 deps/toml11
 
+git clone --quiet https://github.com/fmtlib/fmt deps/fmt
+cd deps/fmt && \
+    git checkout --quiet f5e54359df4c26b6230fc61d38aa294581393084 # 10.1.1
+
+git clone --quiet https://github.com/ToruNiina/toml11 deps/toml11
 cd deps/toml11 && \
     git checkout --quiet dcfe39a783a94e8d52c885e5883a6fbb21529019 # v3.7.1
 
