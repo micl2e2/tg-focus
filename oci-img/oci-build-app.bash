b0b715b48a9942da8d6c68b1fc4a4bd48044ac79
Michael Lee <micl2e2@proton.me>
2023-09-19 22:46:03 +0800

    WIP

--- oci-img/oci-build-app.bash
+++ oci-img/oci-build-app.bash
@@ -1,9 +1,15 @@
 buildah rm build-tgfocus-container
 buildah from --name build-tgfocus-container debian:bookworm-slim
 
+
+
 # install dependencies
 buildah copy --from tdlib-builder-container build-tgfocus-container '/usr/local/include' '/usr/local/include'
 buildah copy --from tdlib-builder-container build-tgfocus-container '/usr/local/lib' '/usr/local/lib'
+
+buildah run build-tgfocus-container -- \
+	sed -i 's/deb\.debian\.org/ftp\.jp\.debian\.org/' /etc/apt/sources.list.d/debian.sources
+
 buildah run build-tgfocus-container -- \
 	apt-get -o Acquire::ForceIPv4=true update
 	# apt-get --quiet update
@@ -11,14 +17,6 @@ buildah run build-tgfocus-container -- \
 	apt-get -o Acquire::ForceIPv4=true install git g++ libssl-dev zlib1g-dev cmake -y
 	# apt-get --quiet install git g++ cmake -y
 
-# buildah run build-tgfocus-container -- \
-# 	mv /lib/x86_64-linux-gnu/libssl.so.3 /lib/x86_64-linux-gnu/xlibssl.so.3
-
-# buildah run build-tgfocus-container -- \
-# 	mv /lib/x86_64-linux-gnu/libcrypto.so.3 /lib/x86_64-linux-gnu/xlibcrypto.so.3
-
-# buildah run build-tgfocus-container -- \
-# 	mv /lib/x86_64-linux-gnu/libz.so.1 /lib/x86_64-linux-gnu/xlibz.so.1
 
 # build tg-focus
 if [[ ! -z $HTTPS_PROXY ]]
@@ -29,8 +27,12 @@ buildah run build-tgfocus-container -- \
 	bash -c "https_proxy=$HTTPS_PROXY git clone --depth=1 https://github.com/micl2e2/tg-focus"
 buildah run build-tgfocus-container -- \
 	bash -c "https_proxy=$HTTPS_PROXY cd tg-focus && bash dl-deps.bash"
+
+# buildah run build-tgfocus-container -- \
+	# bash -c "cd tg-focus && cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_USE_STATIC_LIBS=TRUE -B build && cmake --build build"
+
 buildah run build-tgfocus-container -- \
-	bash -c "cd tg-focus && cmake -DCMAKE_BUILD_TYPE=Release -B build && cmake --build build"
+	bash -c "cd tg-focus && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_LIBRARY_SUFFIXES='.a' -B build && cmake --build build"
 
 
 
