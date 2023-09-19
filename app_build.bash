8109c58dbe1c2137f942760f3f83b25958ac7077
Michael Lee <micl2e2@proton.me>
2023-09-19 12:00:04 +0800

    WIP

--- app_build.bash
+++ app_build.bash
@@ -1,13 +1,14 @@
-
-buildah rm tgfocus-builder-container
-buildah from --name tgfocus-builder-container debian:bullseye-slim
+buildah rm build-tgfocus-container
+buildah from --name build-tgfocus-container debian:bullseye-slim
 
 # install dependencies
-buildah copy --from tdlib-builder-container tgfocus-builder-container '/usr/local/include' '/usr/local/include'
-buildah copy --from tdlib-builder-container tgfocus-builder-container '/usr/local/lib' '/usr/local/lib'
-# buildah run tgfocus-builder-container -- \
+buildah copy --from tdlib-builder-container build-tgfocus-container '/usr/local/include' '/usr/local/include'
+buildah copy --from tdlib-builder-container build-tgfocus-container '/usr/local/lib' '/usr/local/lib'
+buildah run build-tgfocus-container -- \
+	apt-get --quiet update
 	# apt-get --quiet update
-# buildah run tgfocus-builder-container -- \
+buildah run build-tgfocus-container -- \
+	apt-get install git g++ cmake -y
 	# apt-get --quiet install git g++ cmake -y
 
 # build tg-focus
@@ -15,10 +16,12 @@ if [[ ! -z $HTTPS_PROXY ]]
 then
     echo Use proxy $HTTPS_PROXY!
 fi
-# buildah run tgfocus-builder-container -- \
-# 	bash -c "https_proxy=$HTTPS_PROXY git clone --depth=1 https://github.com/micl2e2/tg-focus"
-# buildah run tgfocus-builder-container -- \
-# 	bash -c "source /root/.cargo/env && cd tg-focus && LD_LIBRARY_PATH=/usr/local/lib API_ID=$API_ID API_HASH=$API_HASH cargo build --release"
+buildah run build-tgfocus-container -- \
+	bash -c "https_proxy=$HTTPS_PROXY git clone --depth=1 https://github.com/micl2e2/tg-focus"
+buildah run build-tgfocus-container -- \
+	bash -c "https_proxy=$HTTPS_PROXY cd tg-focus && bash dl-deps.bash"
+buildah run build-tgfocus-container -- \
+	bash -c "cd tg-focus && cmake -B build && cmake --build build"
 
 
 
