14a4219ab132bcfc92ff9a43ed257b780dac63e6
Michael Lee <micl2e2@proton.me>
2024-01-28 16:39:35 +0800

    Remove unnecessary code

--- dev/install-deps-oci-only.bash
+++ dev/install-deps-oci-only.bash
@@ -18,19 +18,6 @@ function dl_3rd_deps
     # !!!
     cd 3rd
 
-    # fmt
-    echo "[INFO] Fetching 'fmt'..."
-    https_proxy=$HTTPS_PROXY git clone --quiet https://github.com/fmtlib/fmt fmt
-    test $? -eq 0 || exit 2
-    echo "[INFO] Fetching 'fmt'...ok"
-
-    # !!!
-    cd fmt && \
-	git checkout --quiet $pick_src_fmt # 10.1.1
-    test $? -eq 0 || exit 2
-    echo "[INFO] 'fmt' source picked"
-    cd .. #  back to 3rd
-
     # toml11
     echo "[INFO] Fetching 'toml11'..."
     https_proxy=$HTTPS_PROXY git clone --quiet https://github.com/ToruNiina/toml11 toml11
@@ -45,8 +32,6 @@ function dl_3rd_deps
     cd .. #  back to 3rd
 }
 
-pick_src_fmt=$(cat dev/pick-src-fmt)
-test $? -eq 0 || exit 1
 pick_src_toml11=$(cat dev/pick-src-toml11)
 test $? -eq 0 || exit 1
 dl_3rd_deps
