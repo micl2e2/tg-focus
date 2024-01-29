49d148700f0eab481fe6a400b60c5ff894b72b88
Michael Lee <micl2e2@proton.me>
2024-01-29 22:56:02 +0800

    Add helper for focusd

--- tf-focusd/tf_focusd.cc
+++ tf-focusd/tf_focusd.cc
@@ -8,19 +8,57 @@
 #include "tf_locale.hh"
 
 void
+print_usage (char *argv[])
+{
+  printf (R"(TG-Focus Daemon
+
+Usage: %s <options>
+
+Available Options:
+  --help           Print this message
+  --verbose        Turn on verbose output
+  --lang           Language preference. When specified, tg-focus will
+                   try to display information using the language specified,
+                   as long as the language is supported by operating system.
+
+                   For example, 'tf-focusd --lang en_US' will use 'English
+                   (United States)' for information displaying. 'tf-focusd
+                   --lang en_US' will use 'English (United Kingdom)' for
+                   information displaying.
+
+                   Currently supported languages:
+
+                   - en_US: English (United States)
+)",
+	  argv[0]);
+}
+
+bool
 handle_opts (int argc, char *argv[])
 {
+  bool ret = false;
   if (argc > 1)
     {
       for (int i = 1; i < argc; i++)
 	{
 	  if (strcmp (argv[i], "--verbose") == 0)
-	    g_log_lv = LogLv::DEBUG;
-	  if (strcmp (argv[i], "--prefer-lang") == 0 && i + 1 < argc
-	      && strlen (argv[i + 1]) >= 5)
-	    tgf::PREFER_LANG = tgf::lang_from_cstr (argv[++i]);
+	    {
+	      g_log_lv = LogLv::DEBUG;
+	    }
+	  else if (strcmp (argv[i], "--lang") == 0 && i + 1 < argc
+		   && strlen (argv[i + 1]) >= 5)
+	    {
+	      tgf::PREFER_LANG = tgf::lang_from_cstr (argv[++i]);
+	    }
+	  else if (strcmp (argv[i], "--help") == 0)
+	    {
+	      print_usage (argv);
+	      ret = true;
+	      break;
+	    }
 	}
     }
+  return ret;
 }
 
 int
@@ -28,7 +66,9 @@ main (int argc, char *argv[])
 {
   using namespace std;
 
-  handle_opts (argc, argv);
+  bool is_handled = handle_opts (argc, argv);
+  if (is_handled)
+    return 0;
 
   if (!tgf::try_ensure_locale ())
     lvlog (LogLv::WARNING, "Available utf8 locales not found");
