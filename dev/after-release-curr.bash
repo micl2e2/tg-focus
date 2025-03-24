d7e35af1c39d92c10573e4193c6a10707a46149f
micl2e2 <micl2e2@proton.me>
2025-03-24 10:28:13 +0000

    remove v2 files

--- dev/after-release-curr.bash
+++
@@ -1,6 +0,0 @@
-
-cp dev/pick-ver-tdlib{,.prev}
-cp dev/pick-src-tdlib{,.prev}
-cp dev/pick-ver-tgfocus{,.prev}
-
-echo '[INFO] curr -> prev done!'
--- tgf-conf/State.cpp
+++
@@ -1,6 +0,0 @@
-#include "State.hpp"
-
-std::atomic<bool> is_login{false};
-std::atomic<bool> is_tdlib_closed{false};
-
-TgfData tgf_data{std::nullopt, false};
