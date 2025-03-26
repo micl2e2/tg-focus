823af57402c2447e8d60bedc9404b9c9ce2e9f07
micl2e2 <micl2e2@proton.me>
2025-03-26 13:06:26 +0000

    WUP

--- ci/prep-libs-alp321-allarch.bash
+++ ci/prep-libs-alp321-allarch.bash
@@ -2,10 +2,12 @@ set -x
 
 # ----------------------------- tdlib -----------------------------
 
-sudo apk add gperf make cmake g++ git zlib-dev zlib-static openssl-dev openssl-libs-static linux-headers wget ccache --quiet
+sudo apk add gperf make cmake g++ git zlib-dev zlib-static openssl-dev \
+     openssl-libs-static linux-headers wget ccache zip --quiet
 [[ $? -eq 0 ]] || exit 255
 
-[[ -d ../3rd/tdlib/.git ]] || $PXY_FRONTEND git clone --depth=1000 https://github.com/tdlib/td ../3rd/tdlib
+[[ -d ../3rd/tdlib/.git ]] || \
+    $PXY_FRONTEND git clone --depth=1000 \https://github.com/tdlib/td ../3rd/tdlib
 [[ $? -eq 0 ]] || exit 255
 
 git --git-dir=../3rd/tdlib/.git --work-tree=../3rd/tdlib reset --hard ${PICK_TDLIB_SRC}
@@ -13,7 +15,8 @@ git --git-dir=../3rd/tdlib/.git --work-tree=../3rd/tdlib reset --hard ${PICK_TDL
 
 # ------------------------------ toml ------------------------------
 
-[[ -d ../3rd/toml11/.git ]] || $PXY_FRONTEND git clone --depth=1000 https://github.com/ToruNiina/toml11 ../3rd/toml11
+[[ -d ../3rd/toml11/.git ]] || \
+    $PXY_FRONTEND git clone --depth=1000 https://github.com/ToruNiina/toml11 ../3rd/toml11
 [[ $? -eq 0 ]] || exit 255
 
 git --git-dir=../3rd/toml11/.git --work-tree=../3rd/toml11 reset --hard ${PICK_LIBTOML_SRC}
