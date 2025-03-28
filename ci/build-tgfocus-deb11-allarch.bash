253d0910b62241bf7f37fc22f466526536514ee4
micl2e2 <micl2e2@proton.me>
2025-03-28 10:16:04 +0800

    ci adjustment

--- ci/build-tgfocus-deb11-allarch.bash
+++ ci/build-tgfocus-deb11-allarch.bash
@@ -1,8 +1,10 @@
 set -x
 
+PICK_BUILD_TYPE=Release
+[[ -v DDD ]] && PICK_BUILD_TYPE=Debug
+
 cmake -S .. -B ../build -DCMAKE_CXX_COMPILER=/usr/local/bin/tgf-g++ -DCMAKE_C_COMPILER=/usr/local/bin/tgf-gcc \
-      -DCMAKE_BUILD_TYPE=Release
-      # -DCMAKE_BUILD_TYPE=Debug
+      -DCMAKE_BUILD_TYPE=$PICK_BUILD_TYPE
 [[ $? -eq 0 ]] || exit 255
 
 LD_LIBRARY_PATH=/usr/local/lib/gcc/${PICK_LIBDIRPREFIX}-linux-gnu/${PICK_GCC_REL}/:/usr/local/lib64/ cmake --build ../build -j$(nproc)
